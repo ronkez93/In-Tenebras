f4a13c1aa4c8dca411186500ba1d12f2f2401b85
Alberto Ronchetti <happyalby@hotmail.it>
2019-02-04 18:16:27 +0100

    aggiunto pathfinding da verificare

--- enemy.py
+++ enemy.py
@@ -3,6 +3,7 @@ from .player import Player
 from .node import Node
 from .Map import Map
 import numpy as np
+import math
 
 
 class Enemy:
@@ -11,15 +12,18 @@ class Enemy:
 
     def __init__(self, playerx, playery):
         global MapSize
-        self.nodes = [[Node() for j in range(MapSize)] for i in range(MapSize)]
-        self.nodes = Map.getAllNode()
-        self.p = Player(playerx, playery)
+        self.map = Map()
+        self.nodes = self.map.getAllNode()
+        self.playerTarget = Player(playerx, playery)
         self.tileX = np.random.random_integer(MapSize)
         self.tileY = np.random.random_integer(MapSize)
-        while self.p.distanceTo(self.tileX, self.tileY) < 7:
+        while self.playerTarget.distanceTo(self.tileX, self.tileY) < 7:
             self.tileX = np.random.random_integer(MapSize)
             self.tileY = np.random.random_integer(MapSize)
         self.move1 = False
+        self.GeneratePathfindingGraph()
+        self.currentpath = []
+        self.remainingMovement = 4
 
     def updatePlayerPos(self, playerx, playery):
         self.playerTarget.position(playerx, playery)
@@ -46,4 +50,40 @@ class Enemy:
                 if y < MapSize - 1:
                     self.nodes[x][y].addNeighbour(self.nodes[x][y + 1])
 
+    def setMove(self):
+        self.move1 = True
 
+    def dijsktra(self, x, y):
+        source = self.nodes[self.tileX][self.tileY]
+        target = self.nodes[x][y]
+        nodes = set(self.nodes)
+        dist = {source: 0}
+        prev = {source: None}
+        unvisited = []
+        for node in nodes:
+            if node != source:
+                dist[node] = math.inf
+                prev[node] = None
+            unvisited.append(node)
+        while len(unvisited) > 0:
+            u = None
+            for n in unvisited:
+                if u == None or dist[n] < dist[u]:
+                    u = n
+            if u == target:
+                break
+            unvisited.remove(u)
+            for n in u.getNeighbour():
+                alt = dist[u] + self.map.costToEnter(u.getx, u.gety, n.getx, n.gety)
+                if alt < dist[n]:
+                    dist[n] = alt
+                    prev[n] = u
+        if prev[target] == None:
+            return
+        cpath = []
+        curr = target
+        while curr != None:
+            cpath.append(curr)
+            curr = prev[curr]
+        cpath.reverse()
+        self.currentpath = cpath
--- node.py
+++ node.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Node:
 
-    def __init__(self, x, y, roomID):
+    def __init__(self, x=-1, y=-1, roomID=-1):
         self.x=x
         self.y=y
         self.roomID=roomID
@@ -15,5 +15,20 @@ class Node:
 
     def addNeighbour(self,n):
         self.neighbours.append(n)
+
+    def setxyID(self,x,y,ID):
+        self.x=x
+        self.y=y
+        self.roomID=ID
+
+    def getNeighbour(self):
+        return self.neighbours
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
 
 
