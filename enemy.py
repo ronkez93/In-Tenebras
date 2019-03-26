38f7f9bc01ad68c71320499ada7314a7b77143b9
Alberto Ronchetti <happyalby@hotmail.it>
2019-03-26 16:07:05 +0100

    minor fix

--- enemy.py
+++ enemy.py
@@ -199,7 +199,7 @@ class Enemy:
             spawn = True
             spawnX = np.random.random_integers(self.MapSize)
             spawnY = np.random.random_integers(self.MapSize)
-            for n in range(self.nodes):
+            for n in range(len(self.nodes)):
                 if self.nodes[n].roomID == self.nodes[spawnX][spawnY].roomID or self.nodes[spawnX][spawnY].roomID == self.nodes[self.playerTarget.getX()][self.playerTarget.getY()].roomID:
                     spawn = False
         self.nodes[spawnX][spawnY].setManifestazione(True)
