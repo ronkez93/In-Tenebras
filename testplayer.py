0b3ff4f7987b9e12922570b2aab99d62eb2eebf1
Alberto Ronchetti <happyalby@hotmail.it>
2019-04-06 15:49:54 +0200

    minor fix

--- testplayer.py
+++ testplayer.py
@@ -208,8 +208,11 @@ try:
             player.x=posPlayerX
             player.y=posPlayerY
             nemico.move1=True
+            print("1")
             nemico.updatePlayerPos(player.x, player.y)
+            print("2")
             nemico.update()
+            print("3")
             ser.write(str(nemico.getPos() + ",1"))
             illuminaStanza()
             print("inizio nuovo turno")
