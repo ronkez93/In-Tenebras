6d37d99824f34de4405e9bbf67af8499a7f315fb
Alberto Ronchetti <happyalby@hotmail.it>
2019-04-07 08:52:00 +0200

    minor fix

--- testplayer.py
+++ testplayer.py
@@ -210,15 +210,15 @@ try:
                         print("usato un oggetto")
                         oggetto=ser.readline()
                         print(oggetto)
-                        if oggetto=="04 27 84 EA E6 4C 81" and not piuma:
+                        if oggetto==" 04 27 84 EA E6 4C 81" and not piuma:
                             piuma=True
                             doppioTurno=True
                             print("piuma")
-                        elif oggetto == "04 38 83 BA 90 5B 81" and not fiasca:
+                        elif oggetto == " 04 38 83 BA 90 5B 81" and not fiasca:
                             fiasca=True
                             player.addStamina(3)
                             print("fiasca")
-                        elif oggetto == "04 40 83 BA 90 5B 81" and not specchio:
+                        elif oggetto == " 04 40 83 BA 90 5B 81" and not specchio:
                             ser.write(str(nemico.getPos())+",3;")
                             specchio=True
                             print("specchio")
