c00362fbb7e9cf5a1cfb552fe5425fc586cc1e14
Alberto Ronchetti <happyalby@hotmail.it>
2019-04-07 21:01:21 +0200

    minor fix

--- testplayer.py
+++ testplayer.py
@@ -1,7 +1,32 @@
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
+#In Tenebras: tesi di laurea magistrale
+#Alberto Ronchetti
+#A.A. 2017-2018
+import RPi.GPIO as GPIO
+#from lib_nrf24 import NRF24
+#import spidev
+
+#GPIO.setmode(GPIO.BCM)
+
+#pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+#radio = NRF24(GPIO, spidev.SpiDev())
+#radio.begin(0, 17)
+
+#radio.setPayloadSize(32)
+#radio.setChannel(0x76)
+#radio.setDataRate(NRF24.BR_1MBPS)
+#radio.setPALevel(NRF24.PA_MIN)
+
+#radio.setAutoAck(True)
+#radio.enableDynamicPayloads()
+#radio.enableAckPayload()
+
+#radio.openWritingPipe(pipes[0])
+#radio.openReadingPipe(1, pipes[1])
+#radio.printDetails()
+
+
+
 import time
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
@@ -13,7 +38,6 @@ import Map
 
 # inport GPIO library
 import numpy as np
-import RPi.GPIO as GPIO  # import RPi.GPIO module
 from time import sleep
 import serial  # importa libreria serial lettura valori usb
 
@@ -102,6 +126,10 @@ fiasca=False
 doppioTurno=False
 vittoria=False
 sconfitta=False
+successo=False
+risolte=0
+battito=0
+minTemp=0
 try:
     clearboard()
     # accende led
@@ -150,7 +178,33 @@ try:
         time.sleep(0.1)
     time.sleep(5)
     #####################################################
-    #   inserire controllo battito cardiaco             #
+    #            controllo battito cardiaco             #
+    # while battito==0:
+    #   message=list("0")
+    #   while len(message)<32:
+    #       message.append(0)
+    #   start = time.time()
+    #   radio.write(message)
+    #   print("Sent the message: {}".format(message))
+    #   radio.startListening()
+    #   while not radio.available(0):
+    #       time.sleep(1 / 100)
+    #       if time.time() - start > 2:
+    #           print("Timed out.")
+    #           break
+    #   receivedMessage = []
+    #   radio.read(receivedMessage, radio.getDynamicPayloadSize())
+    #   print("Received: {}".format(receivedMessage))
+    #
+    #   print("traduzione in unicode")
+    #   string = ""
+    #   for n in receivedMessage:
+    #      if (n >= 32 and n <= 126):
+    #           string += chr(n)
+    #   print("messaggio ricevuto e tradotto: {}".format(string))
+    #   battito=int(string)
+    #   radio.stopListening()
+    #
     #####################################################
     while not vittoria or not sconfitta:  # gestione del turno giocatore
         print("gestione turno giocatore")
@@ -162,9 +216,71 @@ try:
         elif nodes[player.y][player.x].manifestazione:
             playerEndTurn = True
             ######################################################################################
-            # inserire prova sensori per risoluzione manifestazione e gestione evento             #
+            # inserire prova sensori per risoluzione manifestazione e gestione evento            #
+            # #######################################
+            # temperatura
+            #########################################
+            # prova=np.random.random_integers(2)
+            # if prova==1:      temperatura
+            #   message=list("1")
+            #   while len(message)<32:
+            #       message.append(0)
+            #   start = time.time()
+            #   radio.write(message)
+            #   print("Sent the message: {}".format(message))
+            #   radio.startListening()
+            #   while not radio.available(0):
+            #       time.sleep(1 / 100)
+            #       if time.time() - start > 2:
+            #           print("Timed out.")
+            #           break
+            #   receivedMessage = []
+            #   radio.read(receivedMessage, radio.getDynamicPayloadSize())
+            #   print("Received: {}".format(receivedMessage))
+            #   print("traduzione in unicode")
+            #   string = ""
+            #   for n in receivedMessage:
+            #      if (n >= 32 and n <= 126):
+            #           string += chr(n)
+            #   print("messaggio ricevuto e tradotto: {}".format(string))
+            #   minTemp=int(string)
+            #   radio.stopListening()
+            #   for i in range(5):
+            #       time.sleep(1)
+            #       message=list("3")
+            #           while len(message)<32:
+            #               message.append(0)
+            #       start = time.time()
+            #           radio.write(message)
+            #           print("Sent the message: {}".format(message))
+            #           radio.startListening()
+            #           while not radio.available(0):
+            #               time.sleep(1 / 100)
+            #               if time.time() - start > 2:
+            #                   print("Timed out.")
+            #                   break
+            #           receivedMessage = []
+            #           radio.read(receivedMessage, radio.getDynamicPayloadSize())
+            #           print("Received: {}".format(receivedMessage))
+            #           print("traduzione in unicode")
+            #           string = ""
+            #           for n in receivedMessage:
+            #               if (n >= 32 and n <= 126):
+            #                   string += chr(n)
+            #           print("messaggio ricevuto e tradotto: {}".format(string))
+            #           temp=int(string)
+            #           if temp>5*0.5*risolte:
+            #               successo=True
+            # #######################################
+            # giroscopio
+            #########################################
+            # if prova==2:
+            #       
             ######################################################################################
-            nemico.risolviManifestazione(player.x, player.y)
+            if successo:
+                successo=False
+                nemico.risolviManifestazione(player.x, player.y)
+                risolte+=1
         else:
             oldPosx = player.x
             oldPosy = player.y
