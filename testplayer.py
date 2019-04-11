7a2581c63bfaf0c55c94f63b253727c5be98a947
Alberto Ronchetti <happyalby@hotmail.it>
2019-04-11 23:27:47 +0200

    minor fix

--- testplayer.py
+++ testplayer.py
@@ -103,7 +103,7 @@ GPIO.setup(6, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)  # aggiungere tutti gli altri pin: 15 totali finali
 
 gpioPin = [2, 3, 4, 14, 15, 17, 18, 27, 22, 23, 24, 25, 5, 6, 12]
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyAMA0', 9600)
 print('Reading MCP3008 values, press Ctrl-C to quit...')
 print('partenza del ciclo di lettura della posizione')
 print('-' * 57)
